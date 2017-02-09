11be754a71774f1fbe6adbd85b6a17653db1caa3
Timo Pudas <timo.pudas@gmail.com>
2017-02-10 00:17:16 +0200

    Testi lisätty Scale-luokalle

--- MidiSooloisti/src/test/java/midisooloisti/pattern/ScaleTest.java
+++ MidiSooloisti/src/test/java/midisooloisti/pattern/ScaleTest.java
@@ -26,5 +26,31 @@ public class ScaleTest {
     public void rightAmountOfChordNotesAvailable() {
         assertEquals(12, scale.getChordNotes().size());
     }
+    
+    @Test
+    public void getNotesReturnsOnlyNotesThatBelongToTheScale() {
+        int[] notes = {0, 2, 4, 5, 7, 9, 11};
+        Scale majorScale = new Scale(42, 88, notes);
+        
+        boolean ok = true;
+        
+        for(int note : majorScale.getNotes()) {
+            boolean flag = false;
+            for(int i = 0; i < notes.length; i++) {
+
+                if(note % 12 == notes[i]) {
+                    flag = true;
+                }
+            }
+            
+            if (!flag) {
+                System.out.println("väärä nuotti: " + note);
+                ok = false;
+                break;
+            }
+        }
+        
+        assertTrue(ok);
+    }
 
 }
