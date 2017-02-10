f13ad36051aedc2243ec8337a76b21ec7131608c
Timo Pudas <timo.pudas@gmail.com>
2017-02-10 20:26:12 +0200

    Scale-luokalle lisää testejä

--- MidiSooloisti/src/test/java/midisooloisti/pattern/ScaleTest.java
+++ MidiSooloisti/src/test/java/midisooloisti/pattern/ScaleTest.java
@@ -44,13 +44,61 @@ public class ScaleTest {
             }
             
             if (!flag) {
-                System.out.println("väärä nuotti: " + note);
                 ok = false;
                 break;
             }
         }
         
         assertTrue(ok);
+    }
+    
+    @Test
+    public void getChordNotesReturnsOnlyNotesThatBelongToTheChord() {
+        int[] notes = {0, 2, 4, 5, 7, 9, 11};
+        Scale majorScale = new Scale(42, 88, notes);
+        
+        boolean ok = true;
+        
+        for(int note : majorScale.getChordNotes()) {
+            boolean flag = false;
+            for(int i = 0; i <= 4; i = i + 2) { // We'll use only 1. 3. and 5.
+
+                if(note % 12 == notes[i]) {
+                    flag = true;
+                }
+            }
+            
+            if (!flag) {
+                ok = false;
+                break;
+            }
+        }
+        assertTrue(ok);
+    }
+    
+    @Test
+    public void closestChordNoteWorks() {
+        assertEquals(60, scale.closestChordNote(61));
+    }
+    
+    @Test
+    public void findIndexOfPitchWorks() {
+        assertEquals(22, scale.findIndexOfPitch(79));
+    }
+    
+    @Test
+    public void findIndexOfClosestChordNoteWorks() {
+        assertEquals(8, scale.findIndexOfClosestChordNote(57));
+    }
+
+    @Test
+    public void findIndexOfPitchInChordNotesWorks() {
+        assertEquals(6, scale.findIndexOfPitchInChordNotes(67));
+    }
+    
+    @Test
+    public void findIndexOfClosestChordNoteInChordNotesWorks() {
+        assertEquals(7, scale.findIndexOfClosestChordNoteInChordNotes(71));
     }
 
 }
